a20c32a06709ac39356fa379c653e7daa17bd5c7
Nathan <nathan.pauchon.38@gmail.com>
2023-09-06 10:49:43 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Providers from "./providers"
+import "@rainbow-me/rainbowkit/styles.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+        </body>
     </html>
   )
 }
