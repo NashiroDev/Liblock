fbee7c2b511562648576553b505ad96c537344fe
Nathan <nathan.pauchon.38@gmail.com>
2023-09-14 22:41:33 +0200

    fix: fetching of a proposal now somehow work

--- src/app/hooks/seekArticles.jsx
+++ src/app/hooks/seekArticles.jsx
@@ -5,48 +5,52 @@
 // uint256 public articleIDCounter;
 "use client"
 
-import { useContractRead, useContractInfiniteReads, paginatedIndexesConfig  } from 'wagmi';
+import { useContractRead, useContractInfiniteReads, paginatedIndexesConfig } from 'wagmi';
 import { useState, useEffect } from "react";
 import tokenContract from "../../../contracts/Proposal.json";
 
 export default function GetArticles() {
     const proposalContract = "0x12eB4a41Dd1E628C147429b797959F416e8eC906"
 
-    const proposalIDCounter = {
-        address: proposalContract,
-        abi: tokenContract.abi,
-        functionName: 'articleIDCounter',
-    };
-
     const proposalReader = {
         address: proposalContract,
         abi: tokenContract.abi,
-        functionName: 'readProposal',
     };
 
     const { data: counterData } = useContractRead({
-        proposalIDCounter
+        address: proposalContract,
+        abi: tokenContract.abi,
+        functionName: 'articleIDCounter',
     });
 
-    const lastArticleId = counterData?.result ?? 0;
-
-    const articleFetcher = (index) => {
-        const proposalId = index;
-        return [
-            {
-                proposalReader,
-                args: [proposalId],
-            },
-        ];
-    };
+    console.log(counterData)
 
     const { data: articleData, fetchNextPage } = useContractInfiniteReads({
         cacheKey: 'articles',
-        readFunction: articleFetcher,
-        paginationConfig: { start: 0, perPage: 10, direction: 'increment' },
+        ...paginatedIndexesConfig(
+            (start) => {
+                return [
+                    {
+                        ...proposalReader,
+                        functionName: 'readProposal',
+                        args: [index],
+                    },
+                ]
+            },
+            { start: 0, perPage: 4, direction: 'increment' },
+        ),
     });
 
-    console.log(articleData)
+    const articles = articleData.pages;
+    let articlesList = []
+
+    for (const article of articles) {
+        if (article[0] !== undefined) {
+            if (article[0].status === 'success') {
+                articlesList.push(article[0].result);
+            };
+        }
+    }
 
     const [tag, setTag] = useState('');
     const [keyword, setKeyword] = useState('');
@@ -64,14 +68,6 @@ export default function GetArticles() {
         setOrder(e.target.value);
     };
 
-    const handleNextPage = () => {
-        setCurrentPage(currentPage + 1);
-    };
-
-    const handlePreviousPage = () => {
-        setCurrentPage(currentPage - 1);
-    };
-
     const handleSubmit = (e) => {
         e.preventDefault();
         // Logic for filtering and searching articles based on the form inputs
@@ -83,6 +79,8 @@ export default function GetArticles() {
     useEffect(() => {
         fetchNextPage();
     }, []);
+
+    console.log(articleData)
 
     return (
         <div className="container">
@@ -126,23 +124,16 @@ export default function GetArticles() {
                         <button type="submit" className="btn btn-secondary mb-4">Search</button>
                     </form>
                 </div>
-                {articleData && articleData.map((article, index) => (
-                    <div key={index}>
-                        {/* Render the content of each article */}
-                        <h2>{article.title}</h2>
-                        <p>{article.content}</p>
-                    </div>
-                ))}
-                {/* {articleData.map((article) => (
-                    <div key={article.id} className="col-3 ms-4 card mt-4">
+                {articlesList.map((result, index) => (
+                    <div key={index} className="col-3 ms-4 card mt-4">
                         <img src="./assets/logo-color.svg" class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">{article.title}</h5>
-                            <p class="card-text">{article.content.slice(0, 150)}</p>
+                        <div className="card-body">
+                            <h5 className="card-title">{result[0]}</h5>
+                            <p className="card-text">{result[1].slice(0, 150)}</p>
                             <a href="#" class="btn btn-primary">Read</a>
                         </div>
                     </div>
-                ))} */}
+                ))}
             </div>
             <div className="d-flex justify-content-center m-4">
                 {/* {currentPage > 1 && (
