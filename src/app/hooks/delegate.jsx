79af836c446a04db58d39d483d429c68b0cba487
Nathan <nathan.pauchon.38@gmail.com>
2023-09-14 17:08:38 +0200

    feat: added read contract for UI completion

--- src/app/hooks/delegate.jsx
+++ src/app/hooks/delegate.jsx
@@ -1,19 +1,54 @@
 "use client"
-import { useAccount } from 'wagmi';
+import { useAccount, useContractReads } from 'wagmi';
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import { useState, useEffect } from "react";
 import tokenContract from "../../../contracts/contract.json";
 
 export default function Delegate() {
+  const libContract = "0xf2c06D8B5986eB79473CFfF70ABfc2E5986F4EB6"
+
   const [address, setAddress] = useState("");
   const { address: connectedUserAddress } = useAccount();
 
   const { config } = usePrepareContractWrite({
-    address: "0xf2c06D8B5986eB79473CFfF70ABfc2E5986F4EB6",
+    address: libContract,
     abi: tokenContract.abi,
     functionName: "delegateFrom",
     args: [connectedUserAddress, address],
   });
+
+  const liblockBalanceOf = {
+    address: libContract,
+    abi: tokenContract.abi,
+    functionName: 'balanceOf',
+    args: [connectedUserAddress],
+  }
+  const liblockGetVotes = {
+    address: libContract,
+    abi: tokenContract.abi,
+    functionName: 'getVotes',
+    args: [connectedUserAddress],
+  }
+  const liblockDelegates = {
+    address: libContract,
+    abi: tokenContract.abi,
+    functionName: 'delegates',
+    args: [connectedUserAddress],
+  }
+
+  const { data } = useContractReads({
+    contracts: [
+      liblockBalanceOf,
+      liblockGetVotes,
+      liblockDelegates,
+    ],
+  });
+
+  const result_0 = (BigInt(data[0].result) / (BigInt(10n) ** BigInt(18n))).toString();
+  const result_1 = (BigInt(data[1].result) / (BigInt(10n) ** BigInt(18n))).toString();
+  const result_2 = data[2].result;
+
+  console.log(data);
 
   const { delegateData, isDelegateLoading, isDelegateSuccess, write } = useContractWrite(config);
 
@@ -40,6 +75,11 @@ export default function Delegate() {
           </button>
         </div>
       </form>
+      <div className="ms-4 mt-4">
+        <h5>Connected address $LIB token balance : {result_0} LIB</h5>
+        <h5>Connected address votes weight : {result_1} LIB</h5>
+        <h5>Connected address current delegatee : {result_2}</h5>
+      </div>
       {isDelegateLoading && <div>Check wallet</div>}
       {isDelegateSuccess && <div>Transaction: {JSON.stringify(delegateData)}</div>}
     </section>
---
+++ src/app/hooks/seekArticles.jsx
@@ -0,0 +1,5 @@
+readProposal(uint256 proposalId)
+
+proposals[proposalId].adopted = true;
+
+uint256 public articleIDCounter;
