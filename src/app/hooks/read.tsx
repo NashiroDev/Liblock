dec702d38de226cdd44f7b07e05b638eeae00f6e
Nathan <nathan.pauchon.38@gmail.com>
2023-09-17 20:20:57 +0200

    fix: add isLoading to avoid undefined errors

--- src/app/hooks/read.tsx
+++ src/app/hooks/read.tsx
@@ -6,12 +6,17 @@ import tokenContract from "../../../contracts/Proposal.json";
 export default function ReadArticle(id:string): any {
     const proposalContract = "0x12eB4a41Dd1E628C147429b797959F416e8eC906"
 
-    const { data: articleData } = useContractRead({
+    const { data: articleData, isLoading } = useContractRead({
         address: proposalContract,
         abi: tokenContract.abi,
         functionName: 'readProposal',
         args: [id]
     });
+
+    if (isLoading) {
+        // Return a loading state or placeholder
+        return ["Loading", "Loading", "Loading"]
+    }
 
     return articleData
 }
