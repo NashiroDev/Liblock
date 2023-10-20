0ea70d6cb6fd9b0fdcb4c9362c34a985961f973d
Nathan <nathan.pauchon.38@gmail.com>
2023-10-20 12:44:32 +0200

    feat: add multiple hooks

---
+++ src/app/hooks/updateAddressDividends.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import distributorAbi from "../../../contracts/Distributor.json";
+
+export default function UpdateAddressDividends() {
+    const distributorContract = "0x"
+
+    const { config } = usePrepareContractWrite({
+        address: distributorContract,
+        abi: distributorAbi.abi,
+        functionName: "updateAddressDividends",
+    });
+
+    const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        write();
+    };
+
+    return (
+        <section className="container mt-4">
+            <h3>Update Address Dividends</h3>
+            <form onSubmit={handleSubmit} className="d-flex justify-content-center">
+                <div className="input-group mb-3">
+                    <button type="submit" disabled={!write} className="btn btn-primary">
+                        Update
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
+}
---
+++ src/app/hooks/updateAddressInheritance.jsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import distributorAbi from "../../../contracts/Distributor.json";
+
+export default function UpdateEpochInheritance() {
+    const distributorContract = "0x"
+
+    const [address, setAddress] = useState("");
+
+    const { config } = usePrepareContractWrite({
+        address: distributorContract,
+        abi: distributorAbi.abi,
+        functionName: "currentEpochInheritance",
+        args: [address]
+    });
+
+    const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        write();
+    };
+
+    return (
+        <section className="container mt-4">
+            <h3>Initiate address inheritance</h3>
+            <form onSubmit={handleSubmit} className="d-flex justify-content-center">
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        placeholder="Type address to inherit from last epoch"
+                        required
+                        className="form-control"
+                    />
+                    <button type="submit" disabled={!write} className="btn btn-primary">
+                        Inherit
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
+}
