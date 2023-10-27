f7bb3f3d676e0be8dbe853ee053f44f9d9abdfee
Nathan <nathan.pauchon.38@gmail.com>
2023-10-27 15:37:26 +0200

    feat(front): add dashboard in navbar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        'MYSQL_HOST': '127.0.0.1',
+        'MYSQL_PORT': '3306',
+        'MYSQL_DATABASE': 'liblock',
+        'MYSQL_USER': 'Nashiro',
+        'MYSQL_PASSWORD': '',
+    }
+}
 
 module.exports = nextConfig
