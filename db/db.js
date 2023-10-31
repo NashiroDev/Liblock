1c7f3c193a2da2b72635a1bcd74b89afa9c3a0ec
Nathan <nathan.pauchon.38@gmail.com>
2023-10-31 15:15:21 +0100

    feat(back): removed ORM, using mysql raw

--- db/db.js
+++ db/db.js
@@ -1,6 +1,25 @@
-const knex = require("knex");
-const knexFile = require("../knexfile.js");
+const mysql = require('mysql2/promise');
 
-const environment = process.env.NODE_ENV || "development";
+console.log(process.env.DB_HOST,process.env.DB_USER,process.env.DB_PASSWORD,process.env.DB_NAME, "totem")
 
-module.exports = knex(knexFile[environment]);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+const query = async (sql, values) => {
+    const connection = await pool.getConnection();
+
+    try {
+        const [results] = await connection.query(sql, values);
+        return results;
+    } catch (error) {
+        throw error;
+    } finally {
+        connection.release();
+    }
+};
+
+module.exports = { query };
--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        'MYSQL_HOST': '127.0.0.1',
-        'MYSQL_PORT': '3306',
-        'MYSQL_DATABASE': 'liblock',
-        'MYSQL_USER': 'Nashiro',
-        'MYSQL_PASSWORD': '',
-    }
-}
+const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+}
